3029aa385ae00eea165bf77585d5a801d5b89fe9
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-05-04 10:12:15 +0100

    Fix id type for all tables.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,11 +1,11 @@
 CREATE TABLE patients (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     date_of_birth DATE NOT NULL
 );
 
 CREATE TABLE invoices (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     total_amount decimal not null,
     generated_at timestamp not null,
     payed_at timestamp not null,
@@ -13,16 +13,28 @@ CREATE TABLE invoices (
 );
 
 CREATE TABLE treatments (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     type VARCHAR(100) NOT NULL,
     name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE invoice_items (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     unit_price decimal not null,
     quantity int not null,
     total_price decimal not null,
     invoice_id int not null,
     treatment_id int not null
 );
+
+CREATE TABLE medical_histories (
+    id SERIAL PRIMARY KEY,
+    admitted_at timestamp not null,
+    patient_id int not null,
+    status varchar(255) not null
+);
+
+ALTER TABLE medical_histories
+    ADD CONSTRAINT patient_id_fk 
+        FOREIGN KEY (patient_id) 
+            REFERENCES patients(id);
