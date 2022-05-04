116aa9764822717201f757ee7071a37b8500b23c
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-05-04 09:51:11 +0100

    Add invoices table.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,11 @@ CREATE TABLE patients (
     name VARCHAR(100) NOT NULL,
     date_of_birth DATE NOT NULL
 );
+
+CREATE TABLE invoices (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    total_amount decimal not null,
+    generated_at timestamp not null,
+    payed_at timestamp not null,
+    medical_history_id int not null
+);
